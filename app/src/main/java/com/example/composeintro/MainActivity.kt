b3fcde128c18ac0e9c0597f9f1a14101c6cf07bf
arjun98 <arjunkandekar14@gmail.com>
2025-04-01 22:11:36 +0530

    modiffer

---
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.composeintro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeintro.ui.theme.ComposeIntroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+           prevfunck()
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Composable
+    @Preview(showSystemUi = true, showBackground = true )
+    fun prevfunck() {
+     Text(
+         text = "Arjun ",
+         color = Color.Red,
+         modifier = Modifier.background(Color.Yellow)
+             .clickable {  }
+             .size(200.dp)
+             .border(4.dp, Color.Black)
+             .clip(CircleShape)
+             .background(Color.Gray)
+
+
+     )
+    }
+
+
+}
